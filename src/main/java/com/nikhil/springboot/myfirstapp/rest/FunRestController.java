150694b10604dbb019bd3130847370019f8fdef3
n1ght-k1ng <dn.reddy@hotmail.com>
2023-12-01 01:54:27 +0530

    test spring

--- src/main/java/com/nikhil/springboot/myfirstapp/rest/FunRestController.java
+++ src/main/java/com/nikhil/springboot/myfirstapp/rest/FunRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
     public class FunRestController{
         @GetMapping("/")
         public String sayHello(){
-            return "hello world!";
+            return "hello world! I'm here testing it";
         }
     }
 
